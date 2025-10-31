38ac7153224ba84334c001fafc370f00c6ebb0f1
zainab perween <zainabperween577@gmail.com>
2025-10-31 19:29:38 +0530

    fixed build path

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use("/api/news", async(req,res)=>{
    
 })
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 });
 
 app.listen(5000,()=> console.log("Server running on port 5000"));
